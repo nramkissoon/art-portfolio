6dfb7eb0d24e8d6cb2b92781849a20a22a76a56a
Nicholas Ramkissoon <nr1610@nyu.edu>
2019-08-14 17:02:16 -0400

    Updates

---
+++ precache-manifest.9f67379b9e27f2469f41edfbf7efb4e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f01ba66200daf77ea319d3d1fea9254",
+    "url": "/art-portfolio/index.html"
+  },
+  {
+    "revision": "05a40a405ba1aba82b7c",
+    "url": "/art-portfolio/static/css/main.e10dc569.chunk.css"
+  },
+  {
+    "revision": "94ae66518a16ec3e2ace",
+    "url": "/art-portfolio/static/js/2.dbaf7d72.chunk.js"
+  },
+  {
+    "revision": "05a40a405ba1aba82b7c",
+    "url": "/art-portfolio/static/js/main.d3a51f6b.chunk.js"
+  },
+  {
+    "revision": "8b3ab8278305737d033e",
+    "url": "/art-portfolio/static/js/runtime~main.4898935c.js"
+  }
+]);
