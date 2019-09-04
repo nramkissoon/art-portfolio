259fd18a61a2649b0a0146515480d9eb8414b605
Nicholas Ramkissoon <nr1610@nyu.edu>
2019-09-04 11:00:10 -0400

    fixed caption bug

--- src/components/generalComponents/FullImageModal.js
+++ src/components/generalComponents/FullImageModal.js
@@ -5,30 +5,55 @@ import PropTypes from 'prop-types';
 // Full image display when ImageThumbnail is clicked
 class FullImageModal extends PureComponent {
 
+  state = {
+    loaded: false
+  }
+
   showLoaded = (e) => {
     e.target.style.display = 'block';
+    this.setState({loaded: true})
   }
 
   render () {
-    return (
-      <dialog
-        className="dialog"
-        open
-        onClick={this.props.handleShow.bind(this)}
-        style={{margin: 0}}
-      >
-        <div className="fullImageContainer">
-        <img className="fullImage"
-          src={process.env.PUBLIC_URL + this.props.imgFilePath}
-          alt={this.props.description}
-          onLoad={this.showLoaded}
-          />
-        </div>
-        <div className="caption">
-          {this.props.description}{' - '}{this.props.year}
-        </div>
-      </dialog>
-    )
+    if (this.state.loaded) {
+      return (
+        <dialog
+          className="dialog"
+          open
+          onClick={this.props.handleShow.bind(this)}
+          style={{margin: 0}}
+        >
+          <div className="fullImageContainer">
+          <img className="fullImage"
+            src={process.env.PUBLIC_URL + this.props.imgFilePath}
+            alt={this.props.description}
+            onLoad={this.showLoaded}
+            />
+          </div>
+          <div className="caption">
+            {this.props.description}{' - '}{this.props.year}
+          </div>
+        </dialog>
+      )
+    }
+    else{
+      return (
+        <dialog
+          className="dialog"
+          open
+          onClick={this.props.handleShow.bind(this)}
+          style={{margin: 0}}
+        >
+          <div className="fullImageContainer">
+          <img className="fullImage"
+            src={process.env.PUBLIC_URL + this.props.imgFilePath}
+            alt={this.props.description}
+            onLoad={this.showLoaded}
+            />
+          </div>
+        </dialog>
+      )
+    }
   }
 }
 
