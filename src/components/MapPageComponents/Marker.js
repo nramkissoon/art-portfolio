faec9256816af46addd477fd9a1a8176fc5aa571
Nicholas Ramkissoon <nr1610@nyu.edu>
2019-09-12 21:35:21 -0400

    refactoring

--- src/components/MapPageComponents/Marker.js
+++ src/components/MapPageComponents/Marker.js
@@ -1,37 +1,47 @@
-import React, {PureComponent} from 'react';
+import React, { PureComponent } from 'react';
 import './styles/marker.css';
 import PropTypes from 'prop-types';
 
-//Location Markers for Google Map
-class Marker extends PureComponent{
-
+// Location Markers for Google Map
+class Marker extends PureComponent {
   styleMarker() {
-    let s = "";
-    if (this.props.isNight){
-      s = s.concat("nightMarker");
+    const { isNight, selected, photoSetID } = this.props;
+    let s = '';
+    if (isNight) {
+      s = s.concat('nightMarker');
     } else {
-      s = s.concat("dayMarker"); }
-    if (this.props.photoSetID === this.props.selected) {
-      s = s.concat(" selected")
+      s = s.concat('dayMarker');
+    }
+    if (photoSetID === selected) {
+      s = s.concat(' selected');
     }
     return s;
   }
 
   render() {
+    const { photoSetID, markerOnClick } = this.props;
     return (
-      <div onClick={this.props.markerOnClick.bind(this, this.props.photoSetID)}
-        className={this.styleMarker()}>
-        {this.props.photoSetID}
+      <div
+        onClick={markerOnClick.bind(this, photoSetID)}
+        onKeyPress={markerOnClick.bind(this, photoSetID)}
+        className={this.styleMarker()}
+        role="button"
+        tabIndex={0}
+      >
+        {photoSetID}
       </div>
     );
   }
 }
 
-//PropTypes
+// PropTypes
 Marker.propTypes = {
   isNight: PropTypes.bool.isRequired,
   markerOnClick: PropTypes.func.isRequired,
-  selected: PropTypes.string
-}
+  // eslint-disable-next-line react/require-default-props
+  selected: PropTypes.string,
+  // eslint-disable-next-line react/require-default-props
+  photoSetID: PropTypes.string,
+};
 
 export default Marker;
